252d72a9e3e7a9eb660be1b67ab207f69b71c6ce
zeptional <omarpayman89@gmail.com>
2024-12-08 15:48:52 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,83 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string loadShaderSource(const char* filename) {
+    std::ifstream file(filename);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+GLuint compileShader(GLenum shaderType, const std::string& source) {
+    GLuint shader = glCreateShader(shaderType);
+    const char* cSource = source.c_str();
+    glShaderSource(shader, 1, &cSource, nullptr);
+    glCompileShader(shader);
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "Shader Compilation Failed: " << infoLog << std::endl;
+    }
+    return shader;
+}
+
+GLuint createShaderProgram(const std::string& vertexSource, const std::string& fragmentSource) {
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Program Linking Failed: " << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderProgram;
+}
 
 int main() {
-    std::cout << "Shader app running!" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "GLFW Initialization Failed!" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Shader App", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Window Creation Failed!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewInit();
+
+    std::string vertexSource = loadShaderSource("shader.vert");
+    std::string fragmentSource = loadShaderSource("shader.frag");
+
+    GLuint shaderProgram = createShaderProgram(vertexSource, fragmentSource);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+
+        // Render your objects here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
